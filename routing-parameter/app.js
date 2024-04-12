c8191c8568010a98631a2525e104ae87f2046643
Yousuf Borno <yousuf@gmail.com>
2024-04-12 14:32:21 -0700

    routing paramenter

---
+++ routing-parameter/app.js
@@ -0,0 +1,31 @@
+// TOPIC - ROUTING PARAMETER
+/*
+Route parameters are named URL segments that are used to capture the values specified at their position in the URL 
+The captured values are populated in the req.params object
+examples -  /student/12            -------- www.examples.com/student/12
+            /product/:category/:id -------- www.example.com/product/mobile/23
+*/
+
+const express = require("express");
+const app = express();
+
+app.get("/:name/:id/:course/:major", (req, res) => {
+  //since we have the route-parameter named as id so the object would be - {id:value}
+  console.log(req.params); // this is actually an object {id:value}
+  const { name, id, course, major } = req.params;
+  res.send(
+    "<h1>Hello " +
+      name +
+      " and has a student id of " +
+      id +
+      " and looking for a " +
+      course +
+      " as a " +
+      major +
+      " </h1>"
+  );
+});
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
