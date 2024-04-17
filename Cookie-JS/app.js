b21abb507aa84e4d2759c80e9e332657b978b01d
Yousuf Borno <yousuf@gmail.com>
2024-04-17 09:51:50 -0700

    Cookies are ready

---
+++ Cookie-JS/app.js
@@ -0,0 +1,8 @@
+//HOW TO CHECK COOKIES
+/* 
+You can see Cookies in Goolge Chrome by following 
+chrome://settings/content/cookies
+Step 1: go to Privacy and Security
+            - Site Settings
+                
+*/
---
+++ Cookie-JS/createCookie.js
@@ -0,0 +1,10 @@
+//HOW TO CREATE COOKIES
+
+/*
+Wehen you assign a string to document.cookie, the browser parses it as a cookie and adds it to its list of cookies
+syntax
+    - document.cookie = "name=value"
+    - document.cookie = "name=value; expires=date; domain=domain; path=path; secure"
+                            OR based on browser support
+    - document.cookie = "name=value; max-age=in-second; domain=domain; path=path; secure"
+*/
