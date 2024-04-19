7ca219cf56e779d536fd56c3963c641caafb15da
Yousuf Borno <yousuf@gmail.com>
2024-04-19 13:35:59 -0700

    Cookie-parser about to complete

---
+++ Cookie/cookie-parser.js
@@ -0,0 +1,9 @@
+//WHAT IS COOKIE-PARSER
+/*
+cookie-parser is a middleware which parses cookies attached to the client request object.
+Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
+*/
+
+//res.cookie() - It is used to set cookie name to value. Th evalue parameter may be a string or object converted to JSON
+//req.cookies() - This property is used to get cookies
+//res.clearCookie()
